ea237a341843fe30a35d43e9a8aa5c900f23968b
gabriela.ferraro.saigg@gmail.com <gabriela.ferraro.saigg@gmail.com@4e5ad49a-9e45-622d-7282-380460121a5b>
2015-01-28 02:21:43 +0000

    set legend loc to best

--- trunk/plots/bestChunk.py
+++ trunk/plots/bestChunk.py
@@ -47,8 +47,7 @@ marker = ''
 linestyle = '-'
 noupMarker = 's'
 walk_dir = inputfolder
-legParams = {'legend.fontsize': 10,
-          'legend.linewidth': 0.4}
+legParams = {'legend.fontsize': 10}
 
 
 alist_filter = ['brown_cluster_v2000_best.txt', 
@@ -160,7 +159,7 @@ plt.ylim([.7,1])
 plt.xlabel('Training size')
 plt.ylabel('F1-Measure')
 
-#plt.rcParams.update(legParams)
+plt.rcParams.update(legParams)
 plt.legend(loc='lower right')
 savefig(outputfile, bbox_inches='tight')
 #plt.show()
