609780973e04dc0a50045afd60b11e3b893faf59
gabriela.ferraro.saigg <gabriela.ferraro.saigg@gmail.com>
2015-02-04 01:36:21 +0000

    add pdf plots

--- plots/getPlots.sh
+++ plots/getPlots.sh
@@ -8,20 +8,20 @@
 ############################
 
 # - POS
-#python bestPOS.py -i ../POS -o bestPOS.png
-python bestPOS.py -i ../eval_results/results/POS -o bestPOS.png
+#python bestPOS.py -i ../POS -o bestPOS.pdf
+python bestPOS.py -i ../eval_results/results/POS -o bestPOS.pdf
 
 #- Chunking
-#python bestChunk.py -i ../chunking -o bestChunking.png
-python bestChunk.py -i ../eval_results/results/chunking -o bestChunking.png 
+#python bestChunk.py -i ../chunking -o bestChunking.pdf
+python bestChunk.py -i ../eval_results/results/chunking -o bestChunking.pdf 
 
 #- NER
-#python bestNER.py -i ../NER -o bestNER.png
-python bestNER.py -i ../eval_results/results/NER -o bestNER.png
+#python bestNER.py -i ../NER -o bestNER.pdf
+python bestNER.py -i ../eval_results/results/NER -o bestNER.pdf
 
 #- MWE 
-#python bestMWE.py -i ../MWEs -o bestMWE.png
-python bestMWE.py -i ../eval_results/results/MWE -o bestMWE.png
+#python bestMWE.py -i ../MWEs -o bestMWE.pdf
+python bestMWE.py -i ../eval_results/results/MWE -o bestMWE.pdf
 
 # * there are no results for chukning unigram!!
 
@@ -30,31 +30,31 @@ python bestMWE.py -i ../eval_results/results/MWE -o bestMWE.png
 ############################
 
 #- POS
-python POS-OOV-IN.py -i ../eval_results/results/POS -o POS-OOV-IN.png
-python POS-OOV-OUT.py -i ../eval_results/results/POS -o POS-OOV-OUT.png
+python POS-OOV-IN.py -i ../eval_results/results/POS -o POS-OOV-IN.pdf
+python POS-OOV-OUT.py -i ../eval_results/results/POS -o POS-OOV-OUT.pdf
 
 #- Chunking
-python ChunkingOOV.py -i ../eval_results/results/chunking -o Chunking-OOV.png
+python ChunkingOOV.py -i ../eval_results/results/chunking -o Chunking-OOV.pdf
 
 #- NER
-python NER-OOV-IN.py -i ../eval_results/results/NER -o NER-OOV-IN.png
-python NER-OOV-OUT.py -i ../eval_results/results/NER -o NER-OOV-OUT.png
+python NER-OOV-IN.py -i ../eval_results/results/NER -o NER-OOV-IN.pdf
+python NER-OOV-OUT.py -i ../eval_results/results/NER -o NER-OOV-OUT.pdf
 
 #- MWE 
-python MWE-OOV.py -i ../eval_results/results/MWE -o MWE-OOV.png
+python MWE-OOV.py -i ../eval_results/results/MWE -o MWE-OOV.pdf
 
 
 ############################
 #MANUAL vs. INCREMENTAL 
 ############################
 
-#python POSmanual-incre.py -i ../POS -o POSmanual-incre.png
+#python POSmanual-incre.py -i ../POS -o POSmanual-incre.pdf
 
-#python CHUmanual-incre.py -i ../chunking -o CHUmanual-incre.png
+#python CHUmanual-incre.py -i ../chunking -o CHUmanual-incre.pdf
 
-#python NERmanual-incre.py -i ../NER -o NERmanual-incre.png
+#python NERmanual-incre.py -i ../NER -o NERmanual-incre.pdf
 
-#python MWEmanual-incre.py -i ../MWEs -o MWEmanual-incre.png
+#python MWEmanual-incre.py -i ../MWEs -o MWEmanual-incre.pdf
 
 
 echo 'I finished :)'
